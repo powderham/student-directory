ba99369bde2e2d75f7f1395a51c15e6de15d89d9
Ed Powderham <edpowderham@outlook.com>
2016-09-16 15:26:29 +0000

    direcctory.rb

--- directory.rb
+++ directory.rb
@@ -1,20 +1,66 @@
 def input_students
+    
+    
+    #Cohort information - REGEX
+    
+    #puts "Enter month number for cohort Jan-Dec 1-12"
+
+    #month = gets.chomp.to_i
+  
+    #until month =~ /^[1-9]|1[0-2]$/
+    #    puts "Enter a month for the cohort 1-12."
+    #    month = gets.chomp.to_i
+    #end
+    
+    #months = [:January,
+    #    :February,
+    #    :March,
+    #    :April,
+    #    :May,
+    #    :June,
+    #    :July,
+    #    :August,
+    #    :September,
+    #    :October,
+    #    :November,
+    #    :December
+    #] 
+    
+
+    #puts "You have chosen the  #{months[month-1]} cohort"
+    
+    #puts ""
+    
     puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
+    puts "To finish, leave the name and hobby blank"
     
     #create an empty array
     students = []
     
     #get the first name
-    name = gets.chomp
+    puts "Enter a name."
+    name = gets.chop
+    
+    puts "Enter a cohort."
+    cohort = gets.chop
+    
+    puts "Enter a hobby."
+    hobby = gets.chomp
     
     #while the name is not empty, repeat this code
     while !name.empty? do
         #add the student hash to the array
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
+        students << {name: name, hobby: hobby,cohort: cohort}
+        puts "Now we have #{students.count} student#{students.count == 1 ? '' : 's'}"
         # get another name from the user
-        name = gets.chomp
+        puts "Enter another name or leave blank to end."
+        name = gets.chop
+        
+        puts "Enter a cohort or leave blank to end."
+        cohort = gets.chop
+        
+        puts "Enter a hobby or leave blank to end."
+        hobby = gets.chop
     end
     
     #return array of the students
@@ -23,21 +69,43 @@ end
     
     
 def print_header
-    puts "The students of Villains Academy"
-    puts "---------------"
+    puts "The students of Villains Academy".center(40)
+    puts "---------------".center(40)
 end
 
 def print(students)
-    students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    
+    #sort students by cohort (alphabetical order)
+    students.sort! {|a,b| a[:cohort] <=> b[:cohort]}
+    
+    #count of students printed
+    current = 0
+    
+    #test whether all students have been iterated over
+    while current < students.count
+
+        #Test if student name begins with a vowel and is less than 12 characters - prints if yes
+        if students[current][:name].start_with?("a","e","i","o","u","A","E","I","O","U") && students[current][:name].length < 12 
+            puts "#{current+1}. #{students[current][:name]}. Likes #{students[current][:hobby]}. (#{students[current][:cohort]} cohort)".center(40)    
+        end
+    
+    #increment the iterator
+    current += 1
+    
     end
 end
 
-def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+    puts "Overall, we have #{students.count} great student#{students.count == 1 ? '' : 's'}".center(40)
 end
 
 students = input_students
-print_header
-print(students)
-print_footer(students)
+
+if students == []
+    puts "No students, terminating."
+else
+    print_header
+    print(students)
+    print_footer(students)   
+end
+
